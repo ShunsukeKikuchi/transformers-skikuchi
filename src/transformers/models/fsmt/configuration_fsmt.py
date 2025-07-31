958d64fa3dd02d6041e66ef5f6cdde11bee39f90
Cyril Vallez <cyril.vallez@gmail.com>
2025-08-01 01:58:26 +0200

    revert

--- src/transformers/models/fsmt/configuration_fsmt.py
+++ src/transformers/models/fsmt/configuration_fsmt.py
@@ -133,11 +133,7 @@ class FSMTConfig(PretrainedConfig):
     ```"""
 
     model_type = "fsmt"
-    attribute_map = {
-        "num_attention_heads": "encoder_attention_heads",
-        "hidden_size": "d_model",
-        "num_hidden_layers": "decoder_layers",
-    }
+    attribute_map = {"num_attention_heads": "encoder_attention_heads", "hidden_size": "d_model"}
 
     # update the defaults from config file
     def __init__(
